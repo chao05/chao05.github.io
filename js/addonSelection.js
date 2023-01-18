c31d1abb4803561b5a2df64d27c9a0e85b1b32cb
Ye <Chao.Ye@hafele.com.cn>
2023-01-18 13:59:38 +0800

    changed the id no. of javascript file

--- js/addonSelection.js
+++ js/addonSelection.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("input").on("keyup", function() {
       let value = $(this).val().toLowerCase();
-      $("#add_on_list li").filter(function() {
+      $("#responsive_list li").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
       });
     });
